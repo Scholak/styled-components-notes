88365f6a3407a37a2d7d7e05474f8ad3e94e7557
scholak <scholaksebahattin@gmail.com>
2023-07-07 20:06:16 +0300

    props notes added using variant prop on styped button cpomponent

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,22 @@ import { StyledButton } from './components/Button/Button.styles'
 
 const App = () => {
   return (
-    <div>
-      <StyledButton>hi</StyledButton>
-    </div>
-  )
+		<div>
+			<StyledButton>default</StyledButton>
+			<br />
+			<br />
+			<StyledButton variant='info'>info</StyledButton>
+			<br />
+			<br />
+			<StyledButton variant='danger'>danger</StyledButton>
+			<br />
+			<br />
+			<StyledButton variant='success'>success</StyledButton>
+			<br />
+			<br />
+			<StyledButton variant='warning'>warning</StyledButton>
+		</div>
+	)
 }
 
 export default App
--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -1,14 +1,46 @@
 import styled from 'styled-components'
 
-export const StyledButton = styled.button`
-  border: none;
-  outline: none;
+interface ButtonProps {
+  variant?: 'info' | 'danger' | 'success' | 'warning'
+}
+
+const buttonVariant = (variant: string | undefined) => {
+
+  let background: string
+  
+  switch (variant) {
+		case 'info':
+			background = '#0284c7'
+			break
+
+		case 'danger':
+			background = '#dc2626'
+			break
+
+		case 'success':
+			background = '#16a34a'
+			break
+
+		case 'warning':
+			background = '#eab308'
+			break
+
+		default:
+			background = '#334155'
+	}
+
+  return background
+}
+
+export const StyledButton = styled.button<ButtonProps>`
+	border: none;
+	outline: none;
 
 	padding: 12px 24px;
 	font-size: 20px;
 	font-weight: 500;
 	border-radius: 8px;
-	background: #095bb8;
-  color: #fff;
-  cursor: pointer;
+	background: ${props => buttonVariant(props.variant)};
+	color: #fff;
+	cursor: pointer;
 `

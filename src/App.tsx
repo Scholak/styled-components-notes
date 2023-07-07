9b7b465e475fc475e263bac67302720fa5dbf5b8
scholak <scholaksebahattin@gmail.com>
2023-07-07 19:58:25 +0300

    first styled component

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyledButton } from './components/Button/Button.styles'
+
+const App = () => {
+  return (
+    <div>
+      <StyledButton>hi</StyledButton>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Button/Button.styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const StyledButton = styled.button`
+  border: none;
+  outline: none;
+
+	padding: 12px 24px;
+	font-size: 20px;
+	font-weight: 500;
+	border-radius: 8px;
+	background: #095bb8;
+  color: #fff;
+  cursor: pointer;
+`
